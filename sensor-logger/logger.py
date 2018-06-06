d987ee67fd264126ca0d131368bd1704cf92a7d6
Darren Ballinger <darren.ballinger@gmail.com>
2018-06-06 20:37:13 +0000

    Create logger to repeatedly log climate stats every 30 seconds

--- sensor-logger/logger.py
+++ sensor-logger/logger.py
@@ -1,9 +1,20 @@
-#!/usr/bin/python
-
-
+#!/usr/bin/python3
+import time
+import os
+from influx import InfluxDB
 from bme280 import *
-degrees,hectopascals,humidity = readBME280All()
-print(degrees)
-print(hectopascals)
-print(humidity)
 
+while True:
+    degrees,hectopascals,humidity = readBME280All()
+    
+    host_pi = os.getenv("HOSTNAME")
+    client = InfluxDB('http://influxdb:8086')
+    client.create_database('environment')
+    
+    iso = time.ctime()
+    
+    client.write('environment', 'temperature', fields={'value': degrees}, tags={'location': 'peveril'})
+    client.write('environment', 'pressure', fields={'value': hectopascals}, tags={'location': 'peveril'})
+    client.write('environment', 'humidity', fields={'value': humidity}, tags={'location': 'peveril'})
+    
+    time.sleep(30)
