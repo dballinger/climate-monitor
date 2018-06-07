27ebd28bcfead5c482791ee0787c604daab0208e
Darren Ballinger <darren.ballinger@gmail.com>
2018-06-07 18:29:17 +0000

    Add env var to specify the physical location of the logger

--- sensor-logger/logger.py
+++ sensor-logger/logger.py
@@ -7,14 +7,14 @@ from bme280 import *
 while True:
     degrees,hectopascals,humidity = readBME280All()
     
-    host_pi = os.getenv("HOSTNAME")
+    location = os.getenv("PHYSICAL_LOCATION")
     client = InfluxDB('http://influxdb:8086')
     client.create_database('environment')
     
     iso = time.ctime()
     
-    client.write('environment', 'temperature', fields={'value': degrees}, tags={'location': 'peveril'})
-    client.write('environment', 'pressure', fields={'value': hectopascals}, tags={'location': 'peveril'})
-    client.write('environment', 'humidity', fields={'value': humidity}, tags={'location': 'peveril'})
+    client.write('environment', 'temperature', fields={'value': degrees}, tags={'location': location})
+    client.write('environment', 'pressure', fields={'value': hectopascals}, tags={'location': location})
+    client.write('environment', 'humidity', fields={'value': humidity}, tags={'location': location})
     
     time.sleep(30)
