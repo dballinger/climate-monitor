c4cf5d8cce76e5b5115c2429661e0e560235fe3f
Darren Ballinger <darren.ballinger@gmail.com>
2018-06-09 17:21:42 +0000

    Parameterise the monitoring host

--- sensor-logger/logger.py
+++ sensor-logger/logger.py
@@ -4,11 +4,21 @@ import os
 from influx import InfluxDB
 from bme280 import *
 
+monitorHost = os.getenv("MONITOR_HOST")
+if monitorHost == None:
+    monitorHost = "influxdb"
+location = os.getenv("PHYSICAL_LOCATION")
+degrees,hectopascals,humidity = readBME280All()
+print("monitor host: " + monitorHost)
+print("location: " + location)
+print("degrees: " + str(degrees))
+print("hectopascals: " + str(hectopascals))
+print("humidity: " + str(humidity))
+
 while True:
     degrees,hectopascals,humidity = readBME280All()
     
-    location = os.getenv("PHYSICAL_LOCATION")
-    client = InfluxDB('http://influxdb:8086')
+    client = InfluxDB('http://' + monitorHost + ':8086')
     client.create_database('environment')
     
     iso = time.ctime()
